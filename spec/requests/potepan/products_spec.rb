630b5f9c2d35ffeefbe3489bea22110fdd4da5f7
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-09 20:47:05 +0900

    空白修正、商品詳細（rspec）の追加、環境変数から取得に変更

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -4,9 +4,36 @@ RSpec.describe "Potepan::Products", type: :request do
   describe "GET /potepan/products/id" do
     let(:product) { create(:product) }
 
-    it "正常にレスポンスを返すこと（商品詳細）" do
+    before do
       get potepan_product_shousai_path(product.id)
+    end
+
+    it "1.正常なレスポンスかどうか？（id取得）" do
+      expect(response).to be_successful
+    end
+
+    it "2.正常にレスポンスの場合、200（ok）を返すこと" do
       expect(response).to have_http_status(200)
+    end
+
+    it "3.商品名が表示されること" do
+      expect(response.body).to include product.name
+    end
+
+    it "4.商品説明文が表示されること" do
+      expect(response.body).to include product.description
+    end
+
+    it "5.商品の値段が表示されること" do
+      expect(response.body).to include product.price.to_s
+    end
+
+    it "6.商品の値段が表示されること（ドル表記：１番目）" do
+      expect(response.body).to include product.prices.where(currency: "USD").first.amount.to_s
+    end
+
+    it "7.商品の画像が表示されること" do
+      expect(response.body).to include product.variant_images.last.to_s
     end
   end
 end
