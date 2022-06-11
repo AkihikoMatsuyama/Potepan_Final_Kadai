805f967fed3100357833bfb8286abdb323c22194
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-11 12:51:44 +0900

    develop_kadai1and2を間違ってmasterにマージしたので新規に作成&アッパーケース形式に変数

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  Base_title = 'BIGBAG Store'.freeze
+  BASE_TITLE = 'BIGBAG Store'.freeze
   def full_title(page_title)
-    page_title.blank? ? Base_title : "#{page_title} - #{Base_title}"
+    page_title.blank? ? BASE_TITLE : "#{page_title} - #{BASE_TITLE}"
   end
 end
