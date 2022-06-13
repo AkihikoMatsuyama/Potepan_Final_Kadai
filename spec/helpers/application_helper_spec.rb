9619280fa8a4ae82f4f1a5f4125c36d1205abfc2
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-13 16:13:01 +0900

    application_helper_specをletを使用する形に変更

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "ページタイトル" do
-    it "トップページのタイトルは、BIGBAG Storeになる" do
-      expect(full_title("")).to eq 'BIGBAG Store'
-      expect(full_title(nil)).to eq 'BIGBAG Store'
+  describe "full_title(product_name)" do
+    subject { helper.full_title(product_name) }
+
+    context "product_nameが空白の場合のページタイトルは、BIGBAG Storeになる" do
+      let(:product_name) { '' }
+      it { is_expected.to eq 'BIGBAG Store' }
     end
 
-    product_name = SecureRandom.alphanumeric(8)
-    it "商品詳細ページのタイトルは、商品名が#{product_name}の場合、#{product_name} - BIGBAG Storeになる" do
-      expect(full_title(product_name)).to eq("#{product_name} - BIGBAG Store")
+    context "product_nameがnilの場合のページタイトルは、BIGBAG Storeになる" do
+      let(:product_name) { nil }
+      it { is_expected.to eq 'BIGBAG Store' }
+    end
+
+    context "product_nameがテストの場合のページタイトルは、テスト - BIGBAG Storeになる" do
+      let(:product_name) { SecureRandom.alphanumeric(8) }
+      it { is_expected.to eq "#{product_name} - BIGBAG Store" }
     end
   end
 end
