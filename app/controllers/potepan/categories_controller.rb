4605e1ca5284d1030457a1e1cf4288976c102cc0
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-17 19:56:24 +0900

    Categoriesページの左側の商品カテゴリー修正のため

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,5 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @category = Spree::Taxon.find(params[:id])
+    # @categories = Spree::Taxon.all
+    @taxonomies = Spree::Taxon.roots
   end
 end
