fc08ec13dcaf4d7baade5eeecb5753ece4ada8ec
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-22 16:02:12 +0900

    N+1問題を解消するため

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxon.roots
-    @products = @taxon.all_products
+    @products = @taxon.all_products.includes(:variant_images, master: :default_price)
   end
 end
