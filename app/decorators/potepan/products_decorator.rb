cab3c01d624fc5c40580924e6d46facfd6b552e8
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-06-30 18:48:23 +0900

    N+1解消のため

--- app/decorators/potepan/products_decorator.rb
+++ app/decorators/potepan/products_decorator.rb
@@ -2,6 +2,11 @@ module Potepan::ProductsDecorator
   Spree::Product.prepend self
 
   def relation_products
-    Spree::Product.in_taxons(taxons).distinct.where.not(id: id)
+    Spree::Product
+      .in_taxons(taxons)
+      .includes(master: [:default_price, :images])
+      .where.not(id: id)
+      .distinct
+      .order(:name)
   end
 end
