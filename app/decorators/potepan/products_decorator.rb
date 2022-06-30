0d5c5edf841b103fe1182465db3cd45e88812ac6
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-01 04:53:45 +0900

    Potepan::ProductsDecorator作成＆メソッド修正のため

--- app/decorators/potepan/products_decorator.rb
+++ app/decorators/potepan/products_decorator.rb
@@ -2,11 +2,11 @@ module Potepan::ProductsDecorator
   Spree::Product.prepend self
 
   def relation_products
-    Spree::Product
-      .in_taxons(taxons)
-      .includes(master: [:default_price, :images])
-      .where.not(id: id)
-      .distinct
-      .order(:name)
+    Spree::Product.
+      in_taxons(taxons).
+      includes(master: [:default_price, :images]).
+      where.not(id: id).
+      distinct.
+      order(:name)
   end
 end
--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -1,9 +1,19 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe Potepan::ProductsDecorator do
-  let(:products) { Potepan::Products.new.extend Potepan::ProductsDecorator }
-  subject { products }
-  it { should be_a Potepan::Products }
+RSpec.describe Potepan::ProductsDecorator, type: :model do
+  describe '商品詳細ページの関連商品の部分' do
+    let!(:taxon) { create(:taxon) }
+    let!(:taxon2) { create(:taxon) }
+    let!(:product) { create(:product, taxons: [taxon, taxon2]) }
+    let!(:related_product) { create(:product, taxons: [taxon, taxon2]) }
+
+    it "関連商品が重複していないこと" do
+      expect(related_product.taxons).to eq product.taxons
+      expect(product.relation_products).to eq product.relation_products.uniq
+    end
+
+    it "関連商品が自分自身の商品を含まないこと" do
+      expect(product.relation_products).not_to contain_exactly(product)
+    end
+  end
 end
