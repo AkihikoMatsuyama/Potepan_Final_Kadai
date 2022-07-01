1631e4427c55d7cf127e56a9f09fe8f2aa67c1ea
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-01 16:38:04 +0900

    関連商品が最大4つになるテストを追加するため

--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -4,16 +4,29 @@ RSpec.describe Potepan::ProductsDecorator, type: :model do
   describe '商品詳細ページの関連商品の部分' do
     let!(:taxon) { create(:taxon) }
     let!(:taxon2) { create(:taxon) }
+    let!(:taxon3) { create(:taxon) }
+    let!(:taxon4) { create(:taxon) }
     let!(:product) { create(:product, taxons: [taxon, taxon2]) }
-    let!(:related_product) { create(:product, taxons: [taxon, taxon2]) }
+    let!(:product2) { create(:product, taxons: [taxon3, taxon4]) }
+    let!(:related_product) { create_list(:product, 5, taxons: [taxon, taxon2]) }
+    let!(:related_product2) { create_list(:product, 2, taxons: [taxon3, taxon4]) }
 
     it "関連商品が重複していないこと" do
-      expect(related_product.taxons).to eq product.taxons
+      expect(related_product.last.taxons).to eq product.taxons
+      expect(related_product2.last.taxons).to eq product2.taxons
       expect(product.relation_products).to eq product.relation_products.uniq
     end
 
     it "関連商品が自分自身の商品を含まないこと" do
       expect(product.relation_products).not_to contain_exactly(product)
+      expect(product2.relation_products).not_to contain_exactly(product2)
+    end
+
+    it "関連商品が最大4つになること" do
+      expect(product.relation_products.count).to eq 5
+      expect(product.relation_products.limit!(4).count).to be_between(1, 4).inclusive
+      expect(product2.relation_products.count).to eq 2
+      expect(product2.relation_products.limit!(4).count).to be_between(1, 4).inclusive
     end
   end
 end
