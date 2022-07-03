53dfc30ede269dc5a9ff8529273e82eba0b54539
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-04 01:23:26 +0900

    商品関連の並び順のテストをするため

--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -4,12 +4,8 @@ RSpec.describe Potepan::ProductsDecorator, type: :model do
   describe '商品詳細ページの関連商品の部分' do
     let(:taxon) { create(:taxon) }
     let(:taxon2) { create(:taxon) }
-    let(:taxon3) { create(:taxon) }
-    let(:taxon4) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon, taxon2]) }
-    let(:product2) { create(:product, taxons: [taxon3, taxon4]) }
     let!(:related_products) { create_list(:product, 5, taxons: [taxon, taxon2]) }
-    let!(:related_products2) { create_list(:product, 2, taxons: [taxon3, taxon4]) }
 
     it "関連商品が重複していないこと" do
       expect(product.relation_products).to eq product.relation_products.uniq
@@ -17,14 +13,10 @@ RSpec.describe Potepan::ProductsDecorator, type: :model do
 
     it "関連商品が自分自身の商品を含まないこと" do
       expect(product.relation_products).not_to include product
-      expect(product2.relation_products).not_to include product2
     end
 
-    it "関連商品が最大で4つになること" do
-      expect(product.relation_products.count).to eq 5
-      expect(product.relation_products.limit(4).count).to be_between(1, 4).inclusive
-      expect(product2.relation_products.count).to eq 2
-      expect(product2.relation_products.limit(4).count).to be_between(1, 4).inclusive
+    it "関連商品の並び順が名前の昇順になっていること" do
+      expect(product.relation_products).to match product.relation_products.order(:name)
     end
   end
 end
