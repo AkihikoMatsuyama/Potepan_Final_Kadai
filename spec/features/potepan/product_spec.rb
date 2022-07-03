b08256538d414d012927f47df230faaf8c9bb7e1
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-03 19:17:43 +0900

    関連商品が存在しない場合のテストをするため

--- spec/features/potepan/product_spec.rb
+++ spec/features/potepan/product_spec.rb
@@ -4,16 +4,24 @@ RSpec.feature "Potepan::Products", type: :feature do
   given(:taxonomy) { create(:taxonomy) }
   given(:taxon) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root.id) }
   given(:taxon2) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+  given(:taxon3) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root.id) }
+  given(:taxon4) { create(:taxon, taxonomy: taxonomy, parent_id: taxonomy.root.id) }
   given(:product) { create(:product, taxons: [taxon, taxon2]) }
-  given(:related_product) { create_list(:product, 5, taxons: [taxon, taxon2]) }
+  given(:product2) { create(:product, taxons: [taxon3, taxon4]) }
+  given(:related_products) { create_list(:product, 5, taxons: [taxon, taxon2]) }
   given(:image) { create(:image) }
 
   background do
     product.images << image
-    related_product.each do |rel_product|
+    related_products.each do |rel_product|
       rel_product.images << create(:image)
     end
     visit potepan_product_path(product.id)
+  end
+
+  scenario "関連商品が存在しない場合、関連商品のCSSのproductBoxが表示されないこと" do
+    visit potepan_product_path(product2.id)
+    expect(page.all(".productBox").count).to eq 0
   end
 
   scenario "一覧ページへ戻るをクリックすると、商品が属しているtaxonに遷移すること" do
@@ -26,7 +34,7 @@ RSpec.feature "Potepan::Products", type: :feature do
   end
 
   scenario "関連商品名をクリックすると、関連商品の詳細ページに遷移すること" do
-    click_link related_product.first.name
-    expect(page).to have_current_path potepan_product_path(related_product.first.id)
+    click_link related_products.first.name
+    expect(page).to have_current_path potepan_product_path(related_products.first.id)
   end
 end
