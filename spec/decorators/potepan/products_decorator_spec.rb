a88babc2a3a2baa77828cc2d7bf45a69ad1c484f
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-07 15:03:28 +0900

    変数を整理するため

--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -4,9 +4,8 @@ RSpec.describe Potepan::ProductsDecorator, type: :model do
   describe '商品詳細ページの関連商品の部分' do
     subject { product.relation_products }
 
-    let(:taxon) { create(:taxon) }
-    let(:taxon2) { create(:taxon) }
-    let!(:product_list) { create_list(:product, 3, taxons: [taxon, taxon2]) }
+    let(:taxon_list) { create_list(:taxon, 2) }
+    let!(:product_list) { create_list(:product, 3, taxons: taxon_list) }
     let!(:product) { product_list.first }
 
     it "関連商品に絞り込めていること" do
