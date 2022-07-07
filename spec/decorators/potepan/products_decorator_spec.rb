b23a8ba33154441b2ab2f5c7d63a8bf43736feb4
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-07 14:39:15 +0900

    テスト内容を最小限にするため

--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -2,31 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Potepan::ProductsDecorator, type: :model do
   describe '商品詳細ページの関連商品の部分' do
+    subject { product.relation_products }
+
     let(:taxon) { create(:taxon) }
     let(:taxon2) { create(:taxon) }
-    let!(:product) { create(:product, taxons: [taxon, taxon2]) }
-    let!(:related_products) { create_list(:product, 2, taxons: [taxon, taxon2]) }
-
-    it "関連商品が重複していないこと" do
-      expect(related_products).to eq related_products.uniq
-      expect(product.relation_products).to eq related_products.uniq
-    end
+    let!(:product_list) { create_list(:product, 3, taxons: [taxon, taxon2]) }
+    let!(:product) { product_list.first }
 
     it "関連商品に絞り込めていること" do
-      expect(Spree::Product.in_taxons(product.taxons).count).to eq 6
-      expect(Spree::Product.in_taxons(product.taxons).where.not(id: product.id)).
-        not_to include product
-      expect(Spree::Product.in_taxons(product.taxons).where.not(id: product.id).distinct).
-        to eq related_products
-      expect(product.relation_products).to eq related_products
+      expect(subject).not_to include product
     end
 
-    it "関連商品が自分自身の商品を含まないこと" do
-      expect(product.relation_products).not_to include product
-    end
-
-    it "関連商品の並び順が名前の昇順になっていること" do
-      expect(product.relation_products).to match product.relation_products.order(:name)
+    it "関連商品の並び順が名前の照準になっていること" do
+      product.name = 'ZET'
+      product.reload
+      expect(subject).to match subject.order(:name)
     end
   end
 end
