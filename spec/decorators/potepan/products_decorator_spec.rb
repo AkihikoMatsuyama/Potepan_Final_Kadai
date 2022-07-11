7f3de6d6fe810034ebc94c5a55fffb6add8cc8cd
Akihiko Matsuyama <wcjmm655_0610@yahoo.co.jp>
2022-07-11 17:24:55 +0900

    タイトル&テスト内容変更のため

--- spec/decorators/potepan/products_decorator_spec.rb
+++ spec/decorators/potepan/products_decorator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Potepan::ProductsDecorator, type: :model do
-  describe '商品詳細ページの関連商品の部分' do
+  describe '#relation_products' do
     subject { product.relation_products }
 
     let(:taxon_list) { create_list(:taxon, 2) }
@@ -9,15 +9,11 @@ RSpec.describe Potepan::ProductsDecorator, type: :model do
     let(:product) { product_list.first }
 
     it "関連商品に絞り込めていること" do
-      expect(subject).not_to include product
+      expect(subject).to match_array [product_list.second, product_list.third]
     end
 
-    it "in_taxons実行前は、関連商品の数は6になっていること" do
-      expect(subject.unscoped.in_taxons(taxon_list).count).to eq 6
-    end
-
-    it "distinct実行前は、関連商品が重複していないこと" do
-      expect(subject.unscoped.distinct).to eq product_list
+    it "メソッドが機能していること" do
+      expect(subject).to match_array Spree::Product.in_taxons(taxon_list).where.not(id: product.id).distinct
     end
   end
 end
